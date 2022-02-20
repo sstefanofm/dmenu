8698fd7ef354c5ae6b6ddd6bb48b85065b2917df
stefa <sstefanofm@gmail.com>
2022-02-20 02:17:00 -0300

    centered

--- config.h
+++ config.h
@@ -1,20 +1,26 @@
 /* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
+static int topbar = 1;      // -b option, if 0, dmenu appears at bottom
+
+static int centered  = 0;   // center with -c option
+static int min_width = 700; // min width when centered
+
+// -fn option overrides fonts[0]; default X11 font or font set
 static const char *fonts[] = {
-	"monospace:size=10"
+	"agave:size=12"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const char *prompt = NULL; // -p  option; prompt to the left of input field 
+
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+	//           =    #fg        #bg
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel]  = { "#eeeeee", "#005577" },
+	[SchemeOut]  = { "#000000", "#00ffff" },
 };
-/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+
+// -l option; if nonzero, dmenu uses vertical list with given number of lines 
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -22,4 +28,6 @@ static unsigned int lines      = 0;
  */
 static const char worddelimiters[] = " ";
 
+// custom border width
 static const unsigned int border_width = 3;
+
--- dmenu.c
+++ dmenu.c
@@ -89,6 +89,17 @@ calcoffsets(void)
 			break;
 }
 
+static int
+max_textw(void)
+{
+  int len = 0;
+  for (struct item *item = items; item && item->text; item++) 
+  {
+    len = MAX(TEXTW(item->text), len);
+  }
+  return len;
+}
+
 static void
 cleanup(void)
 {
@@ -629,6 +640,7 @@ setup(void)
 	bh = drw->fonts->h + 2;
 	lines = MAX(lines, 0);
 	mh = (lines + 1) * bh;
+  promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
 #ifdef XINERAMA
 	i = 0;
 	if (parentwin == root && (info = XineramaQueryScreens(dpy, &n))) {
@@ -658,6 +670,19 @@ setup(void)
 		x = info[i].x_org;
 		y = info[i].y_org + (topbar ? 0 : info[i].height - mh);
 		mw = info[i].width;
+    if (centered) 
+    {
+      mw = MIN(MAX(max_textw() + promptw, min_width), info[i].width);
+      x  = info[i].x_org + ((info[i].width  - mw) / 2);
+      y  = info[i].y_org + ((info[i].height - mh) / 2);
+    }
+    else 
+    {
+      x  = info[i].x_org;
+      y  = info[i].y_org + (topbar ? 0 : info[i].height - mh);
+      mw = info[i].width;
+    }
+
 		XFree(info);
 	} else
 #endif
@@ -668,6 +693,19 @@ setup(void)
 		x = 0;
 		y = topbar ? 0 : wa.height - mh;
 		mw = wa.width;
+
+    if (centered)
+    {
+      mw = MIN(MAX(max_textw() + promptw, min_width), wa.width);
+      x  = (wa.width  - mw) / 2;
+      y  = (wa.height - mh) / 2;
+    }
+    else
+    {
+      x  = 0;
+      y  = topbar ? 0 : wa.height - mh;
+      mw = wa.width;
+    }
 	}
 	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
 	inputw = MIN(inputw, mw/3);
@@ -728,6 +766,8 @@ main(int argc, char *argv[])
 			topbar = 0;
 		else if (!strcmp(argv[i], "-f"))   /* grabs keyboard before reading stdin */
 			fast = 1;
+    else if (!strcmp(argv[i], "-c"))   /* center dmenu */
+      centered = 1;
 		else if (!strcmp(argv[i], "-i")) { /* case-insensitive item matching */
 			fstrncmp = strncasecmp;
 			fstrstr = cistrstr;
