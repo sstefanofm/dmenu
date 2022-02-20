ef8608ddecc92c8d465f66aebefb9b8bff1a5b62
stefa <sstefanofm@gmail.com>
2022-02-20 02:27:49 -0300

    horizon light themed

--- config.h
+++ config.h
@@ -14,9 +14,9 @@ static const char *prompt = NULL; // -p  option; prompt to the left of input fie
 
 static const char *colors[SchemeLast][2] = {
 	//           =    #fg        #bg
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel]  = { "#eeeeee", "#005577" },
-	[SchemeOut]  = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#06060c", "#fdf0ed" },
+	[SchemeSel]  = { "#3fc4de", "#fdf0ed" },
+	[SchemeOut]  = { "#fdf0ed", "#fdf0ed" },
 };
 
 // -l option; if nonzero, dmenu uses vertical list with given number of lines 
