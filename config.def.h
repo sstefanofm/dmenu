19048753409d0ae7686d06e04b45e7e8b6b24adc
stf <sstefanofm@gmail.com>
2022-06-09 18:31:36 -0300

    Onedark themed.

--- config.def.h
+++ config.def.h
@@ -9,13 +9,24 @@ static const char *fonts[] = {
 	"JetBrainsMono Nerd Font Mono:size=11:style:SemiBold"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+/* * onedark * */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#ebdbb2", "#282828" },
-	/* [SchemeSel] = { "#ebdbb2", "#98971a" }, */
-	[SchemeSel] = { "#282828", "#98971a" },
-	[SchemeOut] = { "#1d2021", "#8ec07c" },
+  /* scheme, fg, bg */
+  [SchemeNorm] = { "#abb2bf", "#282c34" },
+  [SchemeSel]  = { "#282c34", "#e06c75" },
+  [SchemeOut]  = { "#282c34", "#b16286" },
 };
+
+/* * gruvbox * */
+/* static const char *colors[SchemeLast][2] = { */
+/* *     fg         bg       * */
+/* 	[SchemeNorm] = { "#ebdbb2", "#282828" }, */
+/* 	/1* [SchemeSel] = { "#ebdbb2", "#98971a" }, *1/ */
+/* 	[SchemeSel] = { "#282828", "#98971a" }, */
+/* 	[SchemeOut] = { "#1d2021", "#8ec07c" }, */
+/* }; */
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
